564f84fd31c9f38a595d0a9fc23b65786d891065
bobbyZ94 <r.zioltkowski@gmail.com>
2023-02-11 03:39:40 +0100

    production ready with correct urls

--- src/server.ts
+++ src/server.ts
@@ -16,6 +16,10 @@ const start = async () => {
   payload.init({
     secret: process.env.PAYLOAD_SECRET,
     mongoURL: process.env.MONGODB_URI,
+    mongoOptions: {
+      user: 'payloadcms',
+      pass: '9889Dakkord2'
+    },
     express: app,
     onInit: () => {
       payload.logger.info(`Payload Admin URL: ${payload.getAdminURL()}`)
